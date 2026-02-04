7aab20dad2f940d65fe05894847b0695ea8be881
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-02-04 18:16:06 -0500

    feat: Update esling and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
+import prettier from "eslint-config-prettier";
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
   },
-]);
+  {
+    files: ["**/*.test.js", "**/*.spec.js", "**/__tests__/**/*.js"],
+    ...jest.configs["flat/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  prettier,
+];
